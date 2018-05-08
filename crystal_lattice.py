1aa821aac37e307496c0f115e5cf53d199cbd13e
Raphael Castro <raphael.dias.castro@gmail.com>
2018-05-08 18:06:00 -0300

    Opitional arguments

---
+++ crystal_lattice.py
@@ -0,0 +1,24 @@
+# Cubic structures
+import numpy as np
+
+ratios = {
+            'Im3m': np.array([np.sqrt(2 / 2), np.sqrt(4 / 2), np.sqrt(6 / 2), np.sqrt(8 / 2), np.sqrt(10 / 2), np.sqrt(12 / 2),
+                     np.sqrt(14 / 2)]),
+            'Pn3m': np.array([np.sqrt(2 / 2), np.sqrt(3 / 2), np.sqrt(4 / 2), np.sqrt(6 / 2), np.sqrt(8 / 2), np.sqrt(9 / 2),
+                     np.sqrt(10 / 2)]),
+            'Ia3d': np.array([np.sqrt(6 / 6), np.sqrt(8 / 6), np.sqrt(14 / 6), np.sqrt(16 / 6), np.sqrt(20 / 6), np.sqrt(22 / 6),
+                     np.sqrt(24 / 6)]),
+            'Fd3m': np.array([np.sqrt(3 / 3), np.sqrt(8 / 3), np.sqrt(11 / 3), np.sqrt(12 / 3), np.sqrt(16 / 3), np.sqrt(19 / 3),
+                     np.sqrt(24 / 3)])
+}
+
+crystals = {
+            'Im3m': np.array([np.sqrt(2), np.sqrt(4), np.sqrt(6), np.sqrt(8), np.sqrt(10), np.sqrt(12),
+                     np.sqrt(14)]),
+            'Pn3m': np.array([np.sqrt(2), np.sqrt(3), np.sqrt(4), np.sqrt(6), np.sqrt(8), np.sqrt(9),
+                     np.sqrt(10)]),
+            'Ia3d': np.array([np.sqrt(6), np.sqrt(8), np.sqrt(14), np.sqrt(16), np.sqrt(20), np.sqrt(22),
+                     np.sqrt(24)]),
+            'Fd3m': np.array([np.sqrt(3), np.sqrt(8), np.sqrt(11), np.sqrt(12), np.sqrt(16), np.sqrt(19),
+                     np.sqrt(24)])
+}
--- saxs_ratio_analyser.py
+++ saxs_ratio_analyser.py
@@ -1,28 +1,22 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from crystal_lattice import *
 
-def saxs_plot_log(files, begin, end, struc, first_peak, cor, graph):
+def saxs_plot_log(files, struc, first_peak, trace_color, begin=0, end=0, graph=True):
+    files = np.array(files)
     if graph==True:
-        crystals = {
-            'Im3m': [np.sqrt(2 / 2), np.sqrt(4 / 2), np.sqrt(6 / 2), np.sqrt(8 / 2), np.sqrt(10 / 2), np.sqrt(12 / 2),
-                     np.sqrt(14 / 2)],
-            'Pn3m': [np.sqrt(2 / 2), np.sqrt(3 / 2), np.sqrt(4 / 2), np.sqrt(6 / 2), np.sqrt(8 / 2), np.sqrt(9 / 2),
-                     np.sqrt(10 / 2)]}
         for i in range(len(files)):
             data = np.genfromtxt(files[i], skip_header=begin, skip_footer=end, usecols=(0,1), names=['q','I'])
             plt.plot(data['q'], np.log10(data['I'])-i, "-", lw=1)
-        if struc in crystals:
-            for index in crystals[struc]:
-                plt.axvline(x=first_peak * index, ls='-.', color=cor, lw=0.5)
+        if struc in ratios:
+            for index in ratios[struc]:
+                plt.axvline(x=first_peak * index, ls='-.', color=trace_color, lw=0.5)
         plt.xlabel('q [nm^(-1)]')
         plt.ylabel('log(I) [a.u.]')
         plt.yticks([])
         plt.show()
+        print(2*np.pi*crystals[struc][0]/first_peak)
     else:
         return
 
-print('test')
-saxs_plot_log(['ra_10LA_1_norm_minus_agua_7_norm.dat'],20,100,'Pn3m',1.3, 'k', False)
-saxs_plot_log(['ra_10LH_1_norm_minus_agua_7_norm.dat'],20,100,'Pn3m',1.2, 'r', False)
-
-# teste
+saxs_plot_log(['ra_10LA_1_norm_minus_agua_7_norm.dat'], 'Pn3m', 1.3, 'k', begin=20, end=100, graph=True)
+saxs_plot_log(['ra_10LH_1_norm_minus_agua_7_norm.dat'], 'Fd3m', 0.91, 'r', begin=20, end=100, graph=True)
